e8eb015af94f4b028662325a4e7cf156cb85d177
jbsainteve <jbsainteve@yahoo.fr>
2022-10-22 19:19:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Fruit from "./components/Fruit";
+import FruitForm from "./components/FruitForm";
 
 function App() {
+  // state (état, données)
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" }
+  ]);
+
+  // comportements
+  const handleDelete = (id) => {
+    //1. copie du state
+    const fruitsCopy = [...fruits];
+
+    //2. manipuler mon state
+    const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+
+    //3. modifier mon state avec le setter
+    setFruits(fruitsCopyUpdated);
+  };
+
+  const handleAdd = (fruitAAjouter) => {
+    //1. copie du state
+    const fruitsCopy = [...fruits];
+
+    //2. manipulation sur la copie du state
+    fruitsCopy.push(fruitAAjouter);
+
+    //3. modifier le state avec le setter
+    setFruits(fruitsCopy);
+  };
+
+  const afficherFruitPrefere = (fruitNom) => {
+    alert(`J'aime trop ce fruit: ${fruitNom}`);
+  };
+
+  const essaiGrappeJSX = 
+  <div>
+    <Fruit />
+    <Fruit />
+  </div>;
+  
+  // affichage (render)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <Fruit
+            fruitInfo={fruit}
+            // onClick={() => afficherFruitPrefere(fruit.nom)}
+            onClick={() => handleDelete(fruit.id)}
+            key={fruit.id}
+          />
+        ))}
+      </ul>
+      <FruitForm handleAdd={handleAdd} />
     </div>
   );
 }
 
 export default App;
+
+// Gestion du formulaire
+// 1. création du formulaire
+// 2. soumission du formulaire (onSubmit / handleSubmit)
+// 3. collecte des données du formulaire
+// 3a. méthode 1: documentGetElementById "React" (useRef)
+// 3b. méthode 2: sync descendante / ascendante (onChange / handleChange)
+
---
+++ src/components/Fruit.jsx
@@ -0,0 +1,15 @@
+export default function Fruit({ fruitInfo, onClick }) {
+    // state
+    // const fruitInfo = props.fruitInfo;
+    // const onFruitDelete = props.onFruitDelete;
+  
+    // comportements
+  
+    // affichage (render)
+    return (
+      <li>
+        {fruitInfo.nom} <button onClick={onClick}>X</button>
+      </li>
+    );
+  }
+  
